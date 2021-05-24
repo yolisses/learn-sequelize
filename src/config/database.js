7edd44bfcc1fc88d0533813dba75af6351ff01b6
Ulisses <m.stunik@gmail.com>
2021-05-24 11:06:56 -0300

    set database configuration

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+	dialect: 'postgres',
+	host: 'tuffi.db.elephantsql.com',
+	username: 'bqknnzjq',
+	password: '649AvYQoR25Yb3QnBP9VNugGsFnF5rlU',
+	database: 'bqknnzjq',
+	define: {
+		timeStamps: true,
+		underscored: true,
+	},
+};
