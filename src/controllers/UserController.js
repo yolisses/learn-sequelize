cef00ff3466921e8fe2af24958493a2c2fab9e48
Ulisses <m.stunik@gmail.com>
2021-05-26 09:51:18 -0300

    Add index route

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,6 +1,11 @@
 const User = require('../models/User');
 
 module.exports = {
+	async index(req, res) {
+		const users = await User.findAndCountAll({ limit: 10 });
+		return res.json(users);
+	},
+
 	async store(req, res) {
 		const { name, email } = req.body;
 
--- src/routes.js
+++ src/routes.js
@@ -6,5 +6,6 @@ const routes = express.Router();
 require('./database');
 
 routes.post('/users', UserController.store);
+routes.get('/index', UserController.index);
 
 module.exports = routes;
